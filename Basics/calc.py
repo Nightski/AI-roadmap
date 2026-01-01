2468382b1daf757607d2f42e288929cf2bbc91e4
Nightski <kevinvinsent205@gmail.com>
2026-01-01 13:01:18 +0530

    Final touches

--- Basics/calc.py
+++ Basics/calc.py
@@ -1,14 +1,22 @@
-class calculator:
-    def add(a, b):
-        print(a+b)
-    def sub(a, b):
-        print(a - b)
-    def mult(a, b):
-        print(a * b)
-    def div(a, b):
-        print(a / b)
+def add(a, b):
+    print(a+b)
+def sub(a, b):
+    print(a - b)
+def mult(a, b):
+    print(a * b)
+def div(a, b):
+    print(a / b)
 
 a = int(input("Enter first oprand: "))
 b = int(input("Enter second oparand: "))
 
 m = input("Enter your operation +, -, /, *  : ")
+
+if m == '+':
+    add(a, b)
+elif m == '-':
+    sub(a, b)
+elif m == '*':
+    mult(a, b)
+elif m == '/':
+    div(a, b)
