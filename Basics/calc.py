b9474adc3295e203e28b66d1864ae2a589c56630
Nightski <kevinvinsent205@gmail.com>
2026-01-01 12:56:02 +0530

    updated

--- Basics/calc.py
+++ Basics/calc.py
@@ -1,3 +1,13 @@
+class calculator:
+    def add(a, b):
+        print(a+b)
+    def sub(a, b):
+        print(a - b)
+    def mult(a, b):
+        print(a * b)
+    def div(a, b):
+        print(a / b)
+
 a = int(input("Enter first oprand: "))
 b = int(input("Enter second oparand: "))
 
