00f10ea333c902751d26f6ea935c458d2d334cf1
Nightski <kevinvinsent205@gmail.com>
2026-01-01 13:24:04 +0530

    Edge cases handled

--- Basics/calc.py
+++ Basics/calc.py
@@ -1,26 +1,31 @@
 def add(a, b):
-    print(a+b)
+    return a + b
 def sub(a, b):
-    print(a - b)
+    return a - b
 def mult(a, b):
-    print(a * b)
+    return a * b
 def div(a, b):
-    print(a / b)
+    if b == 0:
+        print("Can't divide by zero...")
+        return
+    return a / b
+
+operations = {
+    '+': add,
+    '-':sub,
+    '*': mult,
+    '/': div
+}
+
 while True:
     try:
         a = int(input("Enter first oprand: "))
         b = int(input("Enter second oparand: "))
-        break
     except ValueError:
         print("Please Enter a valid integer")
+        continue
 
-m = input("Enter your operation +, -, /, *  : ")
+    m = input("Enter your operation +, -, /, *  : ")
 
-if m == '+':
-    add(a, b)
-elif m == '-':
-    sub(a, b)
-elif m == '*':
-    mult(a, b)
-elif m == '/':
-    div(a, b)
+    result = operations[m](a,b)
+    print(result)
