b2ebc05b7142b800103c0854156ccea123b21af2
Nightski <kevinvinsent205@gmail.com>
2026-01-03 17:15:53 +0530

    data location

--- Basics/Data_work/begin_data.py
+++ Basics/Data_work/begin_data.py
@@ -1,3 +1,3 @@
 import pandas as pd
 
-df = pd.read_csv(r"C:\Users\kevin vinsent\Downloads\Titanic-Dataset.csv")
+df = pd.read_csv(r"C:\Users\kevin vinsent\Desktop\kevin\AI-roadmap\Basics\Data_work\Titanic-Dataset.csv")
