d4342f1de70347aebb95f83536602e08bbefcd5e
Nightski <kevinvinsent205@gmail.com>
2026-01-03 15:16:06 +0530

    Getting to know list and dicts

---
+++ Basics/LiDi.py
@@ -0,0 +1,7 @@
+info = [{"name" : "Krish", "English" : 89, "Maths" : 99, "science" : 88},
+        {"name" : "Arinjay", "English" : 80, "Maths" : 78, "science" : 80},
+        {"name" : "Kevin", "English" : 99, "Maths" : 65, "science" : 82},
+        {"name" : "Pranjal", "English" : 78, "Maths" : 84, "science" : 78},
+        {"name" : "Tushar", "English" : 64, "Maths" : 89, "science" : 85},
+        ]
+
