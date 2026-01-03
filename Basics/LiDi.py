fe3efe4a2a56b972a466bbd6d534fb1ab2e6b175
Nightski <kevinvinsent205@gmail.com>
2026-01-03 15:31:03 +0530

    More functions

--- Basics/LiDi.py
+++ Basics/LiDi.py
@@ -4,7 +4,18 @@ info = [{"name" : "Krish", "English" : 89, "Maths" : 99, "science" : 88},
         {"name" : "Pranjal", "English" : 78, "Maths" : 84, "science" : 78},
         {"name" : "Tushar", "English" : 64, "Maths" : 89, "science" : 85},
         ]
-
+print("Individual Average")
 for i in info:
     avg = (i["English"] + i["Maths"] + i["science"]) / 3
     print(i["name"] + " " , avg)
+print()
+
+eng, mt, sc = 0,0,0
+for i in info:
+    eng += i["English"]
+    mt += i["Maths"]
+    sc += i["science"]
+print("Class Average")
+print("Science: ",sc/5)
+print("English: ",eng/5)
+print("Maths: ",mt/5)
