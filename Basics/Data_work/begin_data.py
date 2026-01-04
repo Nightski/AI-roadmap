2e00152e2c4de04efec8d65395a33dd88adb985d
Nightski <kevinvinsent205@gmail.com>
2026-01-04 13:28:59 +0530

    Learning

--- Basics/Data_work/begin_data.py
+++ Basics/Data_work/begin_data.py
@@ -1,3 +1,7 @@
 import pandas as pd
 
 df = pd.read_csv(r"C:\Users\kevin vinsent\Desktop\kevin\AI-roadmap\Basics\Data_work\Titanic-Dataset.csv")
+change = {"male" : 1, "female" : 0}
+print(df.columns)
+df = df['Sex'].replace(change).infer_objects(copy=False)
+print(df.head(3))
