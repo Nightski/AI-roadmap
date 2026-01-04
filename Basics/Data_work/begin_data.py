30ee8232aebfaf15c0dabcfabe70281ab3c45a9c
Nightski <kevinvinsent205@gmail.com>
2026-01-04 13:48:37 +0530

    More

--- Basics/Data_work/begin_data.py
+++ Basics/Data_work/begin_data.py
@@ -1,7 +1,10 @@
 import pandas as pd
+pd.set_option('future.no_silent_downcasting', True)
 
 df = pd.read_csv(r"C:\Users\kevin vinsent\Desktop\kevin\AI-roadmap\Basics\Data_work\Titanic-Dataset.csv")
 change = {"male" : 1, "female" : 0}
-print(df.columns)
-df = df['Sex'].replace(change).infer_objects(copy=False)
-print(df.head(3))
+
+df['Sex'] = df['Sex'].replace(change).infer_objects(copy=False)
+
+dead = df['Survived'].value_counts()
+print(dead)
