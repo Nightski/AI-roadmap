e57cd2f168b324ed2477e16723e3df8f030bf89d
Nightski <kevinvinsent205@gmail.com>
2026-01-05 18:22:28 +0530

    Trained the model

--- MLwork/first.py
+++ MLwork/first.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
 pd.set_option('future.no_silent_downcasting', True)
 
 df = pd.read_csv(r"C:\Users\kevin vinsent\Desktop\kevin\AI-roadmap\Basics\Data_work\Titanic_cleaned.csv")
@@ -12,3 +15,12 @@ features = {
 target = 'Survived'
 maping = {"male":1,"female":0}
 df['Sex'] = df['Sex'].replace(maping).infer_objects(copy=False)
+
+x = df[features]
+y = df[target]
+
+xtrain, xtest, ytrain, ytest = train_test_split(x,y,test_size=0.3,random_state=42)
+
+model = LogisticRegression(max_iter=1000)
+model.fit(xtrain, ytrain)
+
