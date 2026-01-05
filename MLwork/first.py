0f8a3f475ef3dc8046223e0ad62c5df984ce2975
Nightski <kevinvinsent205@gmail.com>
2026-01-05 18:16:00 +0530

    yo

--- MLwork/first.py
+++ MLwork/first.py
@@ -1,4 +1,5 @@
 import pandas as pd
+pd.set_option('future.no_silent_downcasting', True)
 
 df = pd.read_csv(r"C:\Users\kevin vinsent\Desktop\kevin\AI-roadmap\Basics\Data_work\Titanic_cleaned.csv")
 features = {
@@ -9,3 +10,5 @@ features = {
     'Isalone'
 }
 target = 'Survived'
+maping = {"male":1,"female":0}
+df['Sex'] = df['Sex'].replace(maping).infer_objects(copy=False)
