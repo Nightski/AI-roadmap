90097e6d0f2b359271b3d0ba715dbd53713b8e88
Nightski <kevinvinsent205@gmail.com>
2026-01-06 13:48:49 +0530

    Decision tree added

--- MLwork/first.py
+++ MLwork/first.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, classification_report
 pd.set_option('future.no_silent_downcasting', True)
 
@@ -21,9 +22,16 @@ y = df[target]
 
 xtrain, xtest, ytrain, ytest = train_test_split(x,y,test_size=0.3,random_state=42)
 
+#Logistic regression
 model = LogisticRegression(max_iter=1000)
 model.fit(xtrain, ytrain)
+#Decision tree
+tree = DecisionTreeClassifier(max_depth=5,random_state=43)
+tree.fit(xtrain,ytrain)
 
 ypred = model.predict(xtest)
-print("Acc: ", accuracy_score(ytest, ypred))
+print("Acc: ", accuracy_score(ytest, ypred) * 100)
 print(classification_report(ytest, ypred))
+
+tree_pred = tree.predict(xtest)
+print("ACC of tree: ",accuracy_score(ytest,tree_pred) * 100)
