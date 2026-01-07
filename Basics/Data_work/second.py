718bba1f67f235325e125045e167a95923a748c9
Nightski <kevinvinsent205@gmail.com>
2026-01-07 18:50:48 +0530

    adding new

--- Basics/Data_work/second.py
+++ Basics/Data_work/second.py
@@ -1,3 +1,5 @@
 import pandas as pd
 
 df = pd.read_csv(r"C:\Users\kevin vinsent\Downloads\house_prices.csv")
+df['date'] = pd.to_datetime(df['date'],format="%Y%m%dT%H%M%S").dt.normalize()
+print(df['date'].dtype)
