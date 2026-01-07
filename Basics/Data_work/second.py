997d373955ad2b116cc3ecb35e93859c299e6c55
Nightski <kevinvinsent205@gmail.com>
2026-01-07 19:03:22 +0530

    cleaning data

--- Basics/Data_work/second.py
+++ Basics/Data_work/second.py
@@ -1,5 +1,17 @@
 import pandas as pd
+pd.set_option('future.no_silent_downcasting', True)
 
 df = pd.read_csv(r"C:\Users\kevin vinsent\Downloads\house_prices.csv")
 df['date'] = pd.to_datetime(df['date'],format="%Y%m%dT%H%M%S").dt.normalize()
-print(df['date'].dtype)
+
+features = [
+    'bedrooms',
+    'floors',
+    'bathrooms',
+    'waterfront'
+    'condition'
+]
+df['waterfront'] = df['waterfront'].replace({"N":0, "Y":1}).infer_objects(copy=False).astype(int)
+print(df['condition'].unique())
+cond = {'Average': 2 ,'Very Good' : 4, 'Good' : 3, 'Poor': 0, 'Fair' : 1}
+df['condition'] = df['condition'].replace(cond).astype(int)
