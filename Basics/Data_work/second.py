80fc67741858e406fc24f0933ad54c733f5abf7f
Nightski <kevinvinsent205@gmail.com>
2026-01-08 12:54:23 +0530

    trained

--- Basics/Data_work/second.py
+++ Basics/Data_work/second.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 pd.set_option('future.no_silent_downcasting', True)
 
 df = pd.read_csv(r"C:\Users\kevin vinsent\Downloads\house_prices.csv")
@@ -8,10 +11,17 @@ features = [
     'bedrooms',
     'floors',
     'bathrooms',
-    'waterfront'
+    'waterfront',
     'condition'
 ]
 df['waterfront'] = df['waterfront'].replace({"N":0, "Y":1}).infer_objects(copy=False).astype(int)
-print(df['condition'].unique())
 cond = {'Average': 2 ,'Very Good' : 4, 'Good' : 3, 'Poor': 0, 'Fair' : 1}
 df['condition'] = df['condition'].replace(cond).infer_objects(copy=False).astype(int)
+output = 'price'
+x = df[features]
+y = df[output]
+xtrain, xtest, ytrain, ytest = train_test_split(x,y,test_size=0.2,random_state=42)
+model = LinearRegression()
+model.fit(xtrain, ytrain)
+ypred = model.predict(xtest)
+print(accuracy_score(ytest, ypred))
