d39b28228fe2919e22757a592ed0eafc12cb9a37
Nightski <kevinvinsent205@gmail.com>
2026-01-08 13:15:15 +0530

    feature engineering 1

--- Basics/Data_work/second.py
+++ Basics/Data_work/second.py
@@ -6,11 +6,19 @@ pd.set_option('future.no_silent_downcasting', True)
 
 df = pd.read_csv(r"C:\Users\kevin vinsent\Downloads\house_prices.csv")
 df['date'] = pd.to_datetime(df['date'],format="%Y%m%dT%H%M%S").dt.normalize()
+
 df['was_renovated'] = (df['yr_renovated'] != 0).astype(int)
 df['reno_age'] = 0
 df.loc[df['yr_renovated'] != 0, 'reno_age'] = (
     df['date'].dt.year - df['yr_renovated']
 )
+
+df['waterfront'] = df['waterfront'].replace({"N":0, "Y":1}).infer_objects(copy=False).astype(int)
+
+cond = {'Average': 2 ,'Very Good' : 4, 'Good' : 3, 'Poor': 0, 'Fair' : 1}
+df['condition'] = df['condition'].replace(cond).infer_objects(copy=False).astype(int)
+
+df['floors'] = df['floors'].astype(int)
 
 features = [
     'bedrooms',
@@ -19,10 +27,7 @@ features = [
     'waterfront',
     'condition'
 ]
-df['waterfront'] = df['waterfront'].replace({"N":0, "Y":1}).infer_objects(copy=False).astype(int)
-cond = {'Average': 2 ,'Very Good' : 4, 'Good' : 3, 'Poor': 0, 'Fair' : 1}
-df['condition'] = df['condition'].replace(cond).infer_objects(copy=False).astype(int)
-df['floors'] = df['floors'].astype(int)
+
 x = df[features]
 y = df['price']
 xtrain, xtest, ytrain, ytest = train_test_split(x,y,test_size=0.2,random_state=42)
