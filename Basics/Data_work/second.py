8b41b101c4449e634aa6b7f93d878724c578ab62
Nightski <kevinvinsent205@gmail.com>
2026-01-08 13:03:01 +0530

    bug 1

--- Basics/Data_work/second.py
+++ Basics/Data_work/second.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 pd.set_option('future.no_silent_downcasting', True)
@@ -17,11 +17,11 @@ features = [
 df['waterfront'] = df['waterfront'].replace({"N":0, "Y":1}).infer_objects(copy=False).astype(int)
 cond = {'Average': 2 ,'Very Good' : 4, 'Good' : 3, 'Poor': 0, 'Fair' : 1}
 df['condition'] = df['condition'].replace(cond).infer_objects(copy=False).astype(int)
-output = 'price'
+df['floors'] = df['floors'].astype(int)
 x = df[features]
-y = df[output]
+y = df['price']
 xtrain, xtest, ytrain, ytest = train_test_split(x,y,test_size=0.2,random_state=42)
-model = LinearRegression()
+model = DecisionTreeClassifier(max_depth=3,random_state=42)
 model.fit(xtrain, ytrain)
 ypred = model.predict(xtest)
-print(accuracy_score(ytest, ypred))
+print(accuracy_score(ytest, ypred) * 100)
